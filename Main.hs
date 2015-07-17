769bcc663b7450ffcde7bcb33fce8951bb733e88
Tom van Ommeren <tom.vanommeren@gmail.com>
2015-07-17 16:49:09 +0200

    initial commit

---
+++ Main.hs
@@ -0,0 +1,39 @@
+import Network
+import System.IO
+import System.IO.Error
+import Data.DateTime
+import Control.Concurrent
+
+main :: IO ()
+main = withSocketsDo $ do
+    sock <- listenOn $ PortNumber 4000
+    putStrLn "Listening on port 4000..."
+    listenLoop sock
+
+listenLoop :: Socket -> IO ()
+listenLoop sock = do
+    (handle, _, _) <- accept sock
+    hSetBuffering handle NoBuffering
+    forkIO $ echoLoop handle
+    listenLoop sock
+
+sendMOTD :: Handle -> IO ()
+sendMOTD handle = do
+    hPutStrLn handle "yo yo yo - Edward Scissorhands"
+
+sendDateTime :: Handle -> IO ()
+sendDateTime handle = do
+    time <- getCurrentTime
+    hPutStrLn handle $ formatDateTime "%c" time
+
+echoLoop :: Handle -> IO ()
+echoLoop handle = do
+    input <- tryIOError (hGetLine handle)
+    case input of
+         Left e ->
+             if isEOFError e
+                then return ()
+                else ioError e
+         Right text -> do
+            hPutStrLn handle text
+            echoLoop handle
